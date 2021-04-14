0791d1c5f71e364fd0ea2f887a91a795aabe7ddf
André Cordeiro <2170972@my.ipleiria.pt>
2021-04-14 21:44:22 +0100

    Client testing

--- Client/src/test/java/pt/tecnico/sec/hdlt/client/AppTest.java
+++ Client/src/test/java/pt/tecnico/sec/hdlt/client/AppTest.java
@@ -3,18 +3,11 @@ package pt.tecnico.sec.hdlt.client;
 import static org.junit.Assert.*;
 import static pt.tecnico.sec.hdlt.IOUtils.readUser;
 
-import org.json.simple.parser.ParseException;
 import org.junit.Test;
-import pt.tecnico.sec.hdlt.FileUtils;
-import pt.tecnico.sec.hdlt.Position;
-import pt.tecnico.sec.hdlt.User;
 import pt.tecnico.sec.hdlt.client.communication.UserClient;
 import pt.tecnico.sec.hdlt.client.communication.UserServer;
 import pt.tecnico.sec.hdlt.client.user.Client;
 import pt.tecnico.sec.hdlt.communication.LocationReport;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * Unit test for simple App.
@@ -24,7 +17,7 @@ public class AppTest
     private static final String gridFileLocation = "../grids.output.json";
 
     @Test
-    public void requestProofsSomeClientsDontRespond()
+    public void requestProofsWithEnoughWitnesses()
     {
         Client client1 = new Client(readUser(gridFileLocation, 1));
         UserClient userClient1 = new UserClient();
@@ -37,5 +30,16 @@ public class AppTest
         assertNotNull(locationReport);
     }
 
+    @Test
+    public void requestProofsWithoutEnoughWitnesses()
+    {
+        Client client1 = new Client(readUser(gridFileLocation, 1));
+        UserClient userClient1 = new UserClient();
+        Client client5 = new Client(readUser(gridFileLocation, 5));
+        UserServer userServer5 = new UserServer(client5);
+
+        LocationReport locationReport = userClient1.requestLocationProofs(client1, 1L, 1);
+        assertNotNull(locationReport);
+    }
 
 }
