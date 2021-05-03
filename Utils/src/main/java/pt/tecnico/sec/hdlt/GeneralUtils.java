f9457ea6ae59921377f37d19cb0b95ff6a6565e4
Andre12Lopes <andre.flopes@outlook.pt>
2021-05-04 00:53:13 +0100

    Finished server tests

--- Utils/src/main/java/pt/tecnico/sec/hdlt/GeneralUtils.java
+++ Utils/src/main/java/pt/tecnico/sec/hdlt/GeneralUtils.java
@@ -6,7 +6,7 @@ import java.util.TimeZone;
 public class GeneralUtils {
     private static final long EPOCH_SIZE = 604800; // 60*60*24*7 = 1 week
     public static final int F = 1;
-    public static final int N_SERVERS = (3*F)+1;
+    public static final int N_SERVERS = (3*F) + 1; // 4
     public static final int SERVER_START_PORT = 50050;
     public static final String SERVER_HOST = "localhost";
 
